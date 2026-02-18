47a931f78f88479d3189afe9043679c3f91743fd
jinheeus <84004698+jinheeus@users.noreply.github.com>
2026-02-18 20:59:51 +0900

    Delete Aiffelthon directory

--- Aiffelthon/rag/naive_rag/state.py
+++
@@ -1,9 +0,0 @@
-from typing import List, TypedDict, Dict, Any, Optional
-from langchain_core.documents import Document
-
-class GraphState(TypedDict):
-    question: str
-    documents: List[Document]
-    search_stats: Dict[str, Any]
-    answer: Optional[str]
-    answer_cot: Optional[List[str]]

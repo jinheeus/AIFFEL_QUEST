ea684ea8a82ee65e6627d8bcf54fa67bb1e6c7dc
확인한 유저네임 < >
2026-02-26 11:08:27 +0900

    chore: 불필요한 파일 삭제

--- Aiffelthon/prism_rag/rag/agentic_rag_v1/utils/models.py
+++
@@ -1,9 +0,0 @@
-import torch
-from sentence_transformers import CrossEncoder
-
-device = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
-
-encoder_model = CrossEncoder(
-    "BAAI/bge-reranker-v2-m3", 
-    device=device
-)
